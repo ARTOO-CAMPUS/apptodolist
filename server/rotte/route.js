916e53184740ebe957b4529907a4c3765c9a78d7
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-16 17:35:33 +0100

    rotte

--- server/rotte/route.js
+++ server/rotte/route.js
@@ -4,6 +4,27 @@ module.exports = function (app, express) {
 
 	app.use(bodyparser.json());
 
+	app.use('/bootstrap', express.static(path.join(__dirname, "..", "..", "node_modules", "bootstrap", "dist")));
+
+	//JQUERY
+	app.use('/jquery', express.static(path.join(__dirname, "..", "..", "node_modules", "jquery", "dist")));
+
+	//ANGULAR
+	app.use('/angular', express.static(path.join(__dirname, "..", "..", "node_modules", "angular")));
+
+	//ANGULAR-UI-ROUTER
+	app.use('/angular-ui-router', express.static(path.join(__dirname, "..", "..", "node_modules", "angular-ui-router")));
+
+	//JS
+	app.use('/js', express.static(path.join(__dirname, "..", "..", "client", "js")));
+
+	//CSS
+	app.use('/css', express.static(path.join(__dirname, "..", "..", "client", "css")));
+
+	//ROTTA INDEX
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__dirname, "..", "..", "client", "index.html"));
+	});
 	//rotta per le todolist
 	app.use('/api/todolist', require('./../risorse/todolist'));
 
