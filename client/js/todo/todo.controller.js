d1e440732d2440cf7557e8cb4b114683ec5e9f84
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-17 22:13:43 +0100

    prompt

--- client/js/todo/todo.controller.js
+++ client/js/todo/todo.controller.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('TodoCtrl', function ($scope, TodoSrv) {
+angular.module('app').controller('TodoCtrl', function ($scope, TodoSrv, $mdDialog) {
 
 	$scope.lista = [];
 	TodoSrv.getTodos().then(function (data) {
@@ -46,6 +46,41 @@ angular.module('app').controller('TodoCtrl', function ($scope, TodoSrv) {
 				});
 			})
 	}
+
+	$scope.showPrompt = function (ev) {
+		// Appending dialog to document.body to cover sidenav in docs app
+		var confirm = $mdDialog.prompt()
+			.title('Cosa vuoi fare oggi?')
+			.textContent('Decidi cosa vuoi fare')
+			.placeholder('Proprio niente')
+			.ariaLabel('Booh')
+			.initialValue('Proprio niente')
+			.targetEvent(ev)
+			.ok('Okay!')
+			.cancel('Ci ho ripensato');
+
+		$mdDialog.show(confirm)
+			.then(function (result) {
+				var nuovo = {};
+				nuovo.titolo = result;
+				nuovo.descrizione = "sono pigro";
+				nuovo.data = new Date();
+				return TodoSrv.creaTodo(nuovo)
+			}, function () {
+				$scope.status = 'You didn\'t name your dog.';
+			})
+			.then(function (data) {
+				$scope.nuovo = {};
+				return TodoSrv.getTodos()
+			})
+			.then(function (data) {
+				$scope.lista = data;
+				$scope.lista.forEach(function (el) {
+					el.data = new Date(el.data);
+				});
+			})
+	};
+
 
 
 });
