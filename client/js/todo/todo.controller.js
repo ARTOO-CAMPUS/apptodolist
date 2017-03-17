30df2639863457203b71217d14106b095ac15e29
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-17 12:06:55 +0100

    client fatto

--- client/js/todo/todo.controller.js
+++ client/js/todo/todo.controller.js
@@ -6,5 +6,33 @@ angular.module('app').controller('TodoCtrl', function ($scope, TodoSrv) {
 		console.log(data);
 	});
 
+	$scope.crea = function () {
+		TodoSrv.creaTodo($scope.nuovo)
+			.then(function (data) {
+				$scope.nuovo = {};
+				return TodoSrv.getTodos()
+			}).then(function (data) {
+				$scope.lista = data;
+			})
+	}
+
+	$scope.elimina = function (id) {
+		TodoSrv.deleteTodo(id)
+			.then(function (data) {
+				return TodoSrv.getTodos()
+			}).then(function (data) {
+				$scope.lista = data;
+			});
+	}
+
+	$scope.update = function (id, fatto) {
+		TodoSrv.updateTodo(id, fatto)
+			.then(function (data) {
+				return TodoSrv.getTodos()
+			}).then(function (data) {
+				$scope.lista = data;
+			})
+	}
+
 
 });
--- client/js/todo/todo.service.js
+++ client/js/todo/todo.service.js
@@ -10,14 +10,62 @@ angular.module('app').service('TodoSrv', function ($http) {
 			.then(function (res) {
 				return res.data;
 			}, function (err) {
-				
+
+			})
+			.catch(function (err) {
+				return err;
+			});
+	}
+	var creaTodo = function (nuovo) {
+
+		return $http({
+				method: 'POST',
+				url: 'http://localhost:' + PORT + '/api/todolist',
+				data: nuovo
+			})
+			.then(function (res) {
+				return res.data;
+			}, function (err) {
+
 			})
 			.catch(function (err) {
 				return err;
 			});
 	}
 
+	var deleteTodo = function (id) {
+		return $http({
+			method: "DELETE",
+			url: 'http://localhost:' + PORT + '/api/todolist/' + id,
+		}).then(function (res) {
+			return res.data;
+		}).catch(function (err) {
+			return err;
+		});
+	}
+
+	var updateTodo = function (id, fatto) {
+		return $http({
+			method: "PUT",
+			url: 'http://localhost:' + PORT + '/api/todolist/' + id,
+			data: {
+				fatto: fatto
+			}
+		}).then(function (res) {
+			return res.data;
+		}).catch(function (err) {
+			return err;
+		})
+	}
+
+
+
+
+
 	return {
-		getTodos: getTodos
+		getTodos: getTodos,
+		creaTodo: creaTodo,
+		deleteTodo: deleteTodo,
+		updateTodo: updateTodo
 	}
 });
