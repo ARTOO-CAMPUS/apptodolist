85bc45310438598e0c3f036f6b260eeb0b84456a
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-17 15:53:57 +0100

    test

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,13 @@
 angular.module('app', [
-	'ui.router',
-	// 'ngMaterial',
-]).run(function () {
-	console.log("app is started");
-});
-// .config(function ($mdThemingProvider) {
-// 	$mdThemingProvider.theme('default')
-// 		.primaryPalette('indigo')
-// 		.accentPalette('green')
-// 		.warnPalette('red')
-// 		.backgroundPalette('grey');
-// 	$mdThemingProvider.theme('altTheme')
-// 		.primaryPalette('purple')
-// 		.accentPalette('green')
-// 		.warnPalette('red')
-// 		.backgroundPalette('grey');
-// });
+		'ui.router',
+		'ngMaterial',
+	]).run(function () {
+		console.log("app is started");
+	})
+	.config(function ($mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('indigo')
+			.accentPalette('green')
+			.warnPalette('red')
+			.backgroundPalette('grey');
+	});
